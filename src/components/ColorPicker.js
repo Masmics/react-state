6eada44076e65362c4902da87f7851c5180f1be6
Masmics <cherlocker3113@gmail.com>
2019-05-14 11:58:02 -0700

    state version of color-picker complete with css

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,17 +1,23 @@
 import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
 import styles from './ColorPicker.css';
 
 export default class ColorPicker extends PureComponent {
-  static proptypes = {
-    color: PropTypes.string.isRequired
+  state = {
+    color: ''
+  };
+
+  clickHandler = color => {
+    this.setState({ color });
   }
 
   render() {
+    const { color } = this.state;
     return (
       <section className={styles.ColorPicker}>
-        <h2> Color: </h2>
-        <h3>this.props.color</h3>
+        <button className={styles.red} onClick={this.clickHandler.bind(null, 'red')}>Red</button>
+        <button className={styles.yellow} onClick={this.clickHandler.bind(null, 'yellow')}>Yellow</button>
+        <button className={styles.blue} onClick={this.clickHandler.bind(null, 'blue')}>Blue</button>
+        <div className={styles[color]}></div>
       </section>
     );
   }
